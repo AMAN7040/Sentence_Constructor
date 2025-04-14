71808c9f418ed2d1152fcc1e7ee7a0c6b235ad21
AMAN7040 <amansingh79620@gmail.com>
2025-04-14 22:18:39 +0530

    feat: add navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className="flex justify-center min-w-screen h-16 bg-[#F8F8F8BF] items-center fixed top-0 shadow-lg">
+      <h1 className="text-black text-2xl font-bold">Sentence Construction</h1>
+    </div>
+  );
+};
+
+export default Navbar;
