d6e24fbf88cd1ff8a5288726acc624a0d6cfb3b2
AMAN7040 <amansingh79620@gmail.com>
2025-04-14 16:57:57 +0530

    feat: set up basic routing structure with BrowserRouter and Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
-  return <><p className="text-red-300">
-    PROJECT START</p></>;
-}
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <div className="app-container">
+        <h1 className="text-center text-4xl p-4">Sentence Construction Quiz</h1>
+
+        <Routes>
+         
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
