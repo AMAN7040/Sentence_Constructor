c90201f38d2c8ba69c024ef6403e401147056090
AMAN7040 <amansingh79620@gmail.com>
2025-04-15 11:36:44 +0530

    feat: set up routes for Home and Questions pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,22 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import QuestionsPage from "./pages/QuestionsPage";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <div className="app-container">
-        <h1 className="text-center text-4xl p-4">Sentence Construction Quiz</h1>
-
+      <header>
+        <Navbar />
+      </header>
+      <main>
         <Routes>
-         
+          <Route path="/" element={<HomePage />} />
+          <Route path="/questions" element={<QuestionsPage />} />
         </Routes>
-      </div>
+      </main>
     </Router>
   );
 };
