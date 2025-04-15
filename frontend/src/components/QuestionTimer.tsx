8f24eac999dd25f666ab412707b67d68a45923cd
AMAN7040 <amansingh79620@gmail.com>
2025-04-15 17:36:50 +0530

    fix: reset timer after every question

--- frontend/src/components/QuestionTimer.tsx
+++ frontend/src/components/QuestionTimer.tsx
@@ -3,19 +3,19 @@ import React, { useEffect, useState } from "react";
 interface QuestionTimerProps {
   duration: number;
   onTimeUp: () => void;
-  resetTrigger: number;
+  questionId: string;
 }
 
 const QuestionTimer: React.FC<QuestionTimerProps> = ({
   duration,
   onTimeUp,
-  resetTrigger,
+  questionId,
 }) => {
   const [timeLeft, setTimeLeft] = useState(duration);
 
   useEffect(() => {
     setTimeLeft(duration);
-  }, [resetTrigger]);
+  }, [duration, questionId]);
 
   useEffect(() => {
     if (timeLeft === 0) {
@@ -31,8 +31,12 @@ const QuestionTimer: React.FC<QuestionTimerProps> = ({
   }, [timeLeft, onTimeUp]);
 
   return (
-    <div className="text-[18px] md:text-2xl font-semibold text-neutral8 mb-4">
-      0:{timeLeft}
+    <div
+      className={`text-[18px] md:text-2xl font-semibold text-neutral8 mb-4 ${
+        timeLeft < 11 ? "text-red-700" : ""
+      }`}
+    >
+      0:{timeLeft.toString().padStart(2, "0")}
     </div>
   );
 };
