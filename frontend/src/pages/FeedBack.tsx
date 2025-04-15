30d08f032089f8634baf9163bfb572ecfc70da1e
AMAN7040 <amansingh79620@gmail.com>
2025-04-15 20:08:56 +0530

    feat: Implement Feedback component displaying score percentage and scorecard

--- frontend/src/pages/FeedBack.tsx
+++ frontend/src/pages/FeedBack.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Question } from "../types/question";
 import ScoreCircle from "../components/ScoreCircle";
+import ScoreCard from "../components/ScoreCard";
 
 const FeedBack: React.FC = () => {
   const { state } = useLocation();
@@ -21,26 +22,36 @@ const FeedBack: React.FC = () => {
     : 0;
 
   return (
-    <div className="min-h-screen flex flex-col items-center py-24">
+    <div className="min-h-screen flex flex-col items-center py-24 bg-[#F6F9F9]">
       <div className="flex flex-col items-center gap-y-6 md:gap-y-14">
-      <div className="flex flex-col items-center text-center gap-6">
-        <div className="flex justify-center w-[clamp(200px,40vw,300px)]">
-          <ScoreCircle score={score} total={questions.length} />
-        </div>
-        <p className="text-[clamp(14px,2vw,18px)] text-neutral8 w-[clamp(350px,60vw,800px)] px-4">
-          While you correctly formed several sentences, there are a couple of
-          areas where improvement is needed. Pay close attention to sentence
-          structure and word placement to ensure clarity and correctness. Review
-          your responses below for more details.
-        </p>
-      </div>
-      <Link to={"/"}>
-        <div className="border border-secondary rounded-[8px] w-30 h-10.5 flex justify-center items-center btn z-1 cursor-pointer transition-transform duration-100 ease-out  hover:scale-105">
-          <p className="font-body font-medium text-[16px] text-secondary hover:text-white">
-            Retry Quiz
+        <div className="flex flex-col items-center text-center gap-6">
+          <div className="flex justify-center w-[clamp(200px,40vw,300px)]">
+            <ScoreCircle score={score} total={questions.length} />
+          </div>
+          <p className="text-[clamp(14px,2vw,18px)] text-neutral8 w-[clamp(350px,60vw,800px)] px-4">
+            While you correctly formed several sentences, there are a couple of
+            areas where improvement is needed. Pay close attention to sentence
+            structure and word placement to ensure clarity and correctness.
+            Review your responses below for more details.
           </p>
         </div>
-      </Link>
+        <Link to={"/"}>
+          <div className="border border-secondary rounded-[8px] w-30 h-10.5 flex justify-center items-center btn z-1 cursor-pointer transition-transform duration-100 ease-out  hover:scale-105">
+            <p className="font-body font-medium text-[16px] text-secondary hover:text-white">
+              Retry Quiz
+            </p>
+          </div>
+        </Link>
+      </div>
+      <div className={`mt-10 grid grid-rows-${questions.length} grid-cols-1 gap-10 md:gap-22`}>
+        {questions.map((q: Question, index:number) => (
+          <ScoreCard
+            key={q.questionId}
+            question={q}
+            index={index}
+            userAnswer={selectedWords[q.questionId] || []}
+          />
+        ))}
       </div>
     </div>
   );
