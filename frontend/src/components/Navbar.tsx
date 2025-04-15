bd5235d4c3d09aedefa45d960bbb2e0015acc789
AMAN7040 <amansingh79620@gmail.com>
2025-04-15 20:03:14 +0530

    ui: increased the z-index and back ground for navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="flex justify-center min-w-screen h-16 bg-[#F8F8F8BF] items-center fixed top-0 shadow-lg">
+    <div className="flex justify-center min-w-screen h-16 items-center fixed top-0 shadow-lg z-100 bg-white">
       <h1 className="text-black text-2xl font-bold">Sentence Construction</h1>
     </div>
   );
